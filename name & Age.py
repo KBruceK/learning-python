adc385396e3b3297b7f022f2a2ba5ebeab9f1bf3
KBRUCEB <103435511+KBruceK@users.noreply.github.com>
2022-04-27 12:28:07 +0300

    Add files via upload

---
+++ name & Age.py
@@ -0,0 +1,5 @@
+Name = input('What is your name? ')
+Age = int(input("what is your age," +  Name  + ",in Yrs?"))
+age = (Age * 12)
+
+print("Your name is,"+  Name  + " and you are ", age, "months old")
